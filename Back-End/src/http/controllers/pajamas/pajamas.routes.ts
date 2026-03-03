3725d4b74571692d60f3ce9a25484706e64bdf7b
Gabriel de Medeiros Felisberto <gabrielmfelis@gmail.com>
2026-03-03 16:20:17 -0300

    fix: tinha esquecido de colocar a rota list no pajamas.routes

--- Back-End/src/http/controllers/pajamas/pajamas.routes.ts
+++ Back-End/src/http/controllers/pajamas/pajamas.routes.ts
@@ -3,10 +3,12 @@ import { createPajama } from "./create-pajama.js";
 import { updatePajamaStock } from "./update-pajama.js";
 import { deletePajama } from "./delete-pajama.js";
 import { getPajamaById } from "./get-pajama.js";
+import { listPajamas } from "./list-pajamas.js";
 
 export async function pajamasRoutes(app: FastifyInstance) {
     app.post('/', createPajama)
     app.put('/:publicId', updatePajamaStock)
     app.delete('/:publicId', deletePajama)
     app.get('/:publicId', getPajamaById)
+    app.get('/', listPajamas)
 }
