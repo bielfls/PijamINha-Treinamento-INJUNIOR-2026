8c16bc74ca95902f5f5874fd79f10235db8e28a4
Claudio0101-bit <salgadoclaudio2005@gmail.com>
2026-03-03 12:51:14 -0300

    feat(main.tsx): Import globals.css in main.tsx

--- Front-end/src/main.tsx
+++ Front-end/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import  './reset.css'
 import "./App.css"
+import './globals.css'
 
 createRoot(document.getElementById('root')!).render(
   
