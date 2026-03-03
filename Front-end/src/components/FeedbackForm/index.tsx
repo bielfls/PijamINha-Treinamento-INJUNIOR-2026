3a0663b7d65522241d792e1255cce4c4f7913a2c
Augusto Brigida Silva <augustobrigida060@gmail.com>
2026-03-03 19:34:26 -0300

    feat: adicionando avaliação por estrelas

--- Front-end/src/components/FeedbackForm/index.tsx
+++ Front-end/src/components/FeedbackForm/index.tsx
@@ -1,14 +1,17 @@
+import Stars from '../Rating'
 import style from './styles.module.css'
 
 export default function FeedbackForm(){
-   
+   function handleSubmit(e:React.FormEvent<HTMLFormElement>){
+     e.preventDefault()
+   }
     return(
         <div className={style.load}>
             <div className={style.box}>
                 <h1>Feedback</h1>
                 <p>Fale um pouco sobre a sua experiência com a nossa loja!</p>
             </div>
-            <form className={style.boxtwo}>
+            <form className={style.boxtwo} onSubmit={handleSubmit}>
                 <input 
                     className={style.info}
                     placeholder="Nome completo"
@@ -21,6 +24,7 @@ export default function FeedbackForm(){
                     
                 
                 />
+                <Stars />
                 <div className={style.buttons}>
                     <button className={style.signe}>Enviar</button>
                 </div>
