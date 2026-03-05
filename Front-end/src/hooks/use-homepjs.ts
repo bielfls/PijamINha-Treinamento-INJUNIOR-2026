9224899c1f379524936a5cf19c2d325275a9ecde
João Vitor Mendes <nickelodeonjv@gmail.com>
2026-03-05 20:50:05 -0300

    Refactor useGetPromoProducts function

--- Front-end/src/hooks/use-homepjs.ts
+++ Front-end/src/hooks/use-homepjs.ts
@@ -7,19 +7,11 @@ export function useGetPromoProducts() {
         queryKey: ["promo-products-home"],
         queryFn: async () => {
             const response = await productService.getPajamas();
-            // 1. Acessamos a lista completa
             const allPajamas: Pijama[] = response.data.pajamas;
-            // 2. Filtramos apenas os que estão em promoção
             const Promo = allPajamas.filter((p) => p.onSale === true);
-
-            // 3. Randomizamos a lista (Embaralhar)
-            // O sort com Math.random() - 0.5 reorganiza os itens aleatoriamente
             const escolhidos = Promo.sort(() => Math.random() - 0.5);
-
-            // 4. Retornamos apenas os 3 primeiros da lista embaralhada
             return escolhidos.slice(0, 3);
         },
-        // Dica: se quiser que mude toda vez que o usuário trocar de aba, 
-        // o React Query já faz isso por padrão.
     });
+
 }
