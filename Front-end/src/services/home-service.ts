11e97ae340cc965f473a389770bc34786911cec5
João Vitor Mendes <nickelodeonjv@gmail.com>
2026-03-05 20:52:03 -0300

    Refactor logging in ProductService for clarity

--- Front-end/src/services/home-service.ts
+++ Front-end/src/services/home-service.ts
@@ -4,17 +4,15 @@ import type { GetPajamasResponse } from "../types/home";
 import { httpAdapter } from "../lib/adapter";
 import { apiRoutes } from "../lib/config";
 
-console.log("TESTE DE CONEXÃO NO ENV ->", import.meta.env.VITE_API_URL);
-
 class ProductService extends baseService {
     public async getPajamas(): Promise<HttpResponse<GetPajamasResponse>> {
-        console.log("CHAMANDO API EM ->", import.meta.env.VITE_API_URL);
-        
+        console.log("api: ", import.meta.env.VITE_API_URL);
         return this.execute<void, GetPajamasResponse>({
             method: "GET",
             url: apiRoutes.PRODUCTSHOME,
         });
     }
 }
+
 
 export const productService = new ProductService(httpAdapter);
