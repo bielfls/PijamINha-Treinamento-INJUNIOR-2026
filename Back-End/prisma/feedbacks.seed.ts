8364b419ca7a8bd2e586c17d1358a1c6ef44744b
Lucas Leite Andrade <lucasla@id.uff.br>
2026-03-06 16:21:13 -0300

    Adicionando seeder de feedbacks

---
+++ Back-End/prisma/feedbacks.seed.ts
@@ -0,0 +1,62 @@
+export const feedbacksToCreate =[
+    {
+        id: 1,
+        "name": "Lucas",
+        "description": "Muito bom",
+        "rating": 5
+    },
+    {
+        id: 2,
+        "name": "Isabella",
+        "description": "Adorei",
+        "rating": 5
+    },
+    {
+        id: 3,
+        "name": "Gabriel",
+        "description": "Top",
+        "rating": 4.5
+    },
+    {
+        id: 4,
+        "name": "Augusto",
+        "description": "Brabo",
+        "rating": 5
+    },
+    {
+        id: 5,
+        "name": "Eduardo",
+        "description": "Excelente",
+        "rating": 4.5
+    },
+    {
+        id: 6,
+        "name": "Cláudio",
+        "description": "Gostei muito",
+        "rating": 4
+    },
+    {
+        id: 7,
+        "name": "João Vitor",
+        "description": "Achei bom",
+        "rating": 3.5
+    },
+    {
+        id: 8,
+        "name": "Ayrton",
+        "description": "Mais ou menos",
+        "rating": 3
+    },
+    {
+        id: 9,
+        "name": "Allber",
+        "description": "Não gostei",
+        "rating": 1.5
+    },
+    {
+        id: 10,
+        "name": "Victor",
+        "description": "Odiei",
+        "rating": 0
+    }
+]
