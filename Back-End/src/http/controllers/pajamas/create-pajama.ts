4e4bf27bdda87ceb7dda2c5dfae8536c44e80363
Isabella Direito <isa.martinsj3@gmail.com>
2026-03-06 13:03:00 -0300

    att

--- Back-End/src/http/controllers/pajamas/create-pajama.ts
+++ Back-End/src/http/controllers/pajamas/create-pajama.ts
@@ -5,36 +5,37 @@ import { PajamaPresenter } from "@/http/presenters/pajamas-presenter.js"
 import { makeCreatePajamaUseCase } from "@/use-cases/factories/pajamas/make-create-pajama.js"
 
 export async function createPajama(request: FastifyRequest, reply: FastifyReply) {
-    const createPajamaBodySchema = z.object({
-        name: z.string().min(1).max(100),
-        description: z.string().min(1),
-        image: z.string(),
-        price: z.number(),
-        season: z.string(),
-        type: z.string(),
-        gender: z.string(),
-        favorite: z.boolean(),
-        onSale: z.boolean(),
-        salePercent: z.number().nullable().optional()
-    })
 
-    const {name, description, image, price, season, type, gender, favorite, onSale, salePercent } = createPajamaBodySchema.parse(
-        request.body,
-    )
+        const createPajamaBodySchema = z.object({
+            name: z.string().min(1).max(100),
+            description: z.string().min(1),
+            image: z.string(),
+            price: z.number(),
+            season: z.string(),
+            type: z.string(),
+            gender: z.string(),
+            favorite: z.boolean(),
+            onSale: z.boolean(),
+            salePercent: z.number().nullable().optional()
+        })
 
-    const createPajamaUseCase = makeCreatePajamaUseCase()
-    const { pajama } = await createPajamaUseCase.execute({
-        name,
-        description,
-        image,
-        price,
-        season,
-        type,
-        gender,
-        favorite,
-        onSale,
-        salePercent
-    })
+        const {name, description, image, price, season, type, gender, favorite, onSale, salePercent } = createPajamaBodySchema.parse(
+            request.body,
+        )
 
-    return reply.status(201).send({pajama: PajamaPresenter.toHTTP(pajama)})
+        const createPajamaUseCase = makeCreatePajamaUseCase()
+        const { pajama } = await createPajamaUseCase.execute({
+            name,
+            description,
+            image,
+            price,
+            season,
+            type,
+            gender,
+            favorite,
+            onSale,
+            salePercent
+        })
+
+        return reply.status(201).send({pajama: PajamaPresenter.toHTTP(pajama)})
 }
